88efe2359246c169e17937438a4ef698637cfd1a
10thSSAFY <ssafyiwan@gmail.com>
2023-08-03 00:33:33 +0900

    20230803

---
+++ 20230803/problem_4839.py
@@ -0,0 +1,34 @@
+import sys
+sys.stdin = open('input_4839.txt', 'r')
+
+T = int(input())
+
+for tc in range(1, T+1):
+    P, A, B = map(int, input().split())
+    Al, Bl, Ar, Br = 1, 1, P, P
+
+    while True:
+        Ac = (Ar + Al) // 2
+        Bc = (Br + Bl) // 2
+
+        if Ac == A and Bc == B:
+            result = 0
+            break
+        elif Ac == A:
+            result = 'A'
+            break
+        elif Bc == B:
+            result = 'B'
+            break
+
+        if Ac < A:
+            Al = Ac
+        else:
+            Ar = Ac
+
+        if Bc < B:
+            Bl = Bc
+        else:
+            Br = Bc
+
+    print(f'#{tc} {result}')
---
+++ 20230803/problem_4843.py
@@ -0,0 +1,23 @@
+import sys
+sys.stdin = open('input_4843.txt', 'r')
+
+T = int(input())
+for tc in range(1, T+1):
+    N = int(input())
+    lst = list(map(int, input().split()))
+    
+    sorted_lst = []
+    while True:
+
+        if lst and len(sorted_lst) < 10:
+            sorted_lst.append( str(lst.pop(lst.index(max(lst)))) )
+        else:
+            break
+
+        if lst and len(sorted_lst) < 10:
+            sorted_lst.append( str(lst.pop(lst.index(min(lst)))) )
+        else:
+            break
+    
+    result = (' '.join(sorted_lst))
+    print(f'#{tc} {result}')
