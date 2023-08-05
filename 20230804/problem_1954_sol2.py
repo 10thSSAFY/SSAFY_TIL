b639a11735bac676009b07e8daf43b19966f0b6d
10thSSAFY <ssafyiwan@gmail.com>
2023-08-05 20:53:12 +0900

    20230804

--- 20230804/problem_1954_sol2.py
+++ 20230804/problem_1954_sol2.py
@@ -4,7 +4,6 @@ sys.stdin = open('input_1954.txt', 'r')
 
 T = int(input())
 
-
 dr = [0, 1, 0, -1]
 dc = [1, 0, -1, 0]
 
@@ -16,14 +15,12 @@ for tc in range(1, T+1):
     for value in range(1, N*N+1):
         arr[curR][curC] = value
         
-        newR = curR + dr[d]
-        newC = curC + dc[d]
+        newR, newC = curR + dr[d], curC + dc[d]
         if newR<0 or newR>=N or newC<0 or newC>=N or arr[newR][newC] != 0: 
             d = (d+1) % 4
 
         curR += dr[d]
         curC += dc[d]
-
 
     print(f'#{tc}')
     for i in range(N):
