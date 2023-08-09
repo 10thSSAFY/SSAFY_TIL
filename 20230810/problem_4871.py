20cb663ebdb404adfe3d23d6d366be9dc72f3e43
10thSSAFY <ssafyiwan@gmail.com>
2023-08-10 03:35:25 +0900

    20230810

---
+++ 20230810/problem_4871.py
@@ -0,0 +1,29 @@
+# 그래프 경로
+import sys
+sys.stdin = open('res/input_4817.txt', 'r')
+
+def path(Start, Goal, Graph, Visited):
+    if Start == Goal:
+        return 1
+    for tmp in Graph[Start]:
+        if Visited[tmp] == 'F':
+            Visited[tmp] = 'T'
+            if path(tmp, Goal, Graph, Visited):
+                return 1
+    return 0
+
+
+T = int(input())
+for tc in range(1, T+1):
+    V, E = map(int, input().split())
+    graph = [[] for _ in range(V+1)]
+    visited = ['F'] * (V+1)
+    for _ in range(E):
+        s, e = map(int, input().split())
+        graph[s].append(e)
+
+    S, G = map(int, input().split())
+    visited[S] = 'T'
+    result = path(S, G, graph, visited)
+
+    print(f'#{tc} {result}')
