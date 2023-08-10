a511fcd00849744f46ffdeaf3e0a1a2bb997d26b
10thSSAFY <ssafyiwan@gmail.com>
2023-08-10 23:27:45 +0900

    20230810

--- 20230810/problem_1959.py
+++ 20230810/problem_1959.py
@@ -3,11 +3,21 @@ import sys
 sys.stdin = open('res/input_1959.txt', 'r')
 
 T = int(input())
-for tc in range(1, T+1):
+for t in range(1, T+1):
     N, M = map(int, input().split())
-    lst1 = list(map(int, input().split()))
-    lst2 = list(map(int, input().split()))
-    for i in range(abs(N-M)+1):
-        if N < M:
-            for j in nange()
-    print(f'{tc}')
+    Nlst = list(map(int, input().split()))
+    Mlst = list(map(int, input().split()))
+ 
+    if N > M:  
+        N, M = M, N
+        Nlst, Mlst = Mlst, Nlst
+ 
+    result = 0
+    for i in range(M-N+1):
+        sumV = 0
+        for j in range(N):
+            sumV += Nlst[j] * Mlst[i+j]
+        if result <= sumV:
+            result = sumV
+ 
+    print(f'#{t} {result}')
