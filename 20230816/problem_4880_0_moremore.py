455f9884531b4e08681d548248de900800666c0e
10thSSAFY <ssafyiwan@gmail.com>
2023-08-16 17:57:50 +0900

    20230816

--- 20230816/problem_4880_0_moremore.py
+++ 20230816/problem_4880_0_moremore.py
@@ -4,14 +4,18 @@ sys.stdin = open('res/input_4880.txt', 'r')
 def winner(lst, start, end):
     if end == start:
         return lst[start]
-    if end-start == 1:
-        return lst[start]
-
-    mid = (start + end) // 2
-    winner1 = winner(lst, start, mid)
-    winner2 = winner(lst, mid + 1, end)
-
     
+    elif end-start == 1:
+        if lst[start]-lst[end] == -1 or lst[start]-lst[end] == 2:
+            return lst[start]
+        else:
+            return lst[end]
+
+    else:
+        mid = (start + end) // 2
+        winner1 = winner(lst, start, mid)
+        winner2 = winner(lst, mid + 1, end)
+        w
 
 T = int(input())
 for tc in range(1, T + 1):
