a45dc38fbf2d37e19e54a5c03da5c95a51de8384
10thSSAFY <ssafyiwan@gmail.com>
2023-08-19 16:56:34 +0900

    20230817

--- 20230817/problem_1860_2.py
+++ 20230817/problem_1860_2.py
@@ -7,21 +7,8 @@ for tc in range(1, T+1):
     N, M, K = map(int,input().split())
     nums = sorted(list(map(int,input().split())))
     result = 'Possible'
-    for s in nums:
-        if (s//M)*K - nums.index(s) - 1 < 0:
+    for s in range(len(nums)):
+        if (nums[s]//M)*K - s - 1 < 0:
             result ='Impossible'
             break
     print(f'#{tc} {result}')
-'''
-T = int(input())
-for tc in range(1, T+1):
-    n,m,k = map(int,input().split())
-    nums = sorted(list(map(int,input().split())))
-    time = 1
-    a ='Possible'
-    for s in nums:
-        if (s//m)*k-nums.index(s)-1<0:
-            a ='Impossible'
-            break
-    print(f'#{tc}', a)
-'''
