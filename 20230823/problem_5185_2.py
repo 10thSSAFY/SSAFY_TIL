d76c890a6aba23289eddca9726259d822f8a0e66
10thSSAFY <ssafyiwan@gmail.com>
2023-08-23 22:37:36 +0900

    20230823

--- 20230823/problem_5185_2.py
+++ 20230823/problem_5185_2.py
@@ -4,19 +4,21 @@ sys.stdin = open("res/input_5185.txt", "r")
 
 num = {'A':10, 'B':11, 'C':12,
        'D':13, 'E':14, 'F':15}
-
+ 
 T = int(input())
 for tc in range(1, T+1):
-    N, hx = int, input().split()
+    N, hx = input().split()
+    result = ''
     for n in hx:
         if n in 'ABCDEF':
             n = num[n]
         else:
             n = int(n)
-
+ 
         for i in range(4):
-            if n & (1<<i):
-                print(1, end='')
+            if n & (8>>i):
+                result += '1'
             else:
-                print(0, end='')
-        print()
+                result += '0'
+                 
+    print(f'#{tc} {result}')
