371f31bbe927e6788a5679cd0f472952bbb53c65
10thSSAFY <ssafyiwan@gmail.com>
2023-09-01 00:53:12 +0900

    20230831

--- 20230831/problem_2817.py
+++ 20230831/problem_2817.py
@@ -2,3 +2,27 @@
 import sys
 sys.stdin = open('res/input_2817.txt', 'r')
 
+def f(k, curr_sum):
+    global cnt
+    if k == N:
+        if curr_sum == M:
+            cnt += 1
+        return
+ 
+    if curr_sum > M:
+        return
+ 
+    if curr_sum == M:
+        cnt += 1
+        return
+ 
+    f(k+1, curr_sum + lst[k])
+    f(k+1, curr_sum)
+ 
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    lst = list(map(int, input().split()))
+    cnt = 0
+    f(0, 0)
+    print(f'#{tc} {cnt}')
--- 20230831/problem_4012.py
+++ 20230831/problem_4012.py
@@ -2,14 +2,27 @@
 import sys
 sys.stdin = open('res/input_4012.txt', 'r')
 
+def SNG(lst):
+    res = 0
+    for i in range(len(lst)-1):
+        for j in range(i+1, len(lst)):
+            res += arr[lst[i]][lst[j]] + arr[lst[j]][lst[i]]
+    return res
+
 T = int(input())
-T = 1
 for tc in range(1, T+1):
     N = int(input())
     arr = [list(map(int, input().split())) for _ in range(N)]
-    tmp = []
-    for i in range(N-1):
-        for j in range(i+1, N):
-            tmp.append(arr[i][j]+arr[j][i])
-    print(tmp)
-    print(f'#{tc}')
+    result = 2147483648
+    for i in range(1, 1<<(N-1)):
+        G1=[]
+        G2=[]
+        for j in range(N):
+            if i&(1<<j):
+                G1.append(j)
+            else:
+                G2.append(j)
+        if len(G1) == N//2:
+            result = min(result, abs(SNG(G1)-SNG(G2)))
+
+    print(f'#{tc} {result}')
